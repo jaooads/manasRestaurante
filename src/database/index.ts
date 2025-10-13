c640e3a567b3dffb6c0ef0f10b090715237d8025
jaooads <joaoarthur24062006@gmail.com>
2025-10-13 10:21:22 -0300

    Instalação dependencias e MySql

---
+++ src/database/index.ts
@@ -0,0 +1,24 @@
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const sequelize = new Sequelize(
+    process.env.DB_NAME as string,
+    process.env.DB_USER as string,
+    process.env.DB_PASS as string,
+    {
+        host: process.env.DB_HOST,
+        dialect: "mysql",
+        logging: false,
+    }
+);
+
+export const connectDB = async () => {
+    try {
+        await sequelize.authenticate();
+        console.log("Conexão com o banco de dados estabelecida com sucesso!");
+    } catch (error) {
+        console.error("Erro ao conectar ao banco:", error);
+    }
+};
